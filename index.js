629f196dbf3e2efa84da4848783fcffee79d9d3d
ksed <hydrogeo.coder@gmail.com>
2016-11-18 20:19:31 -0500

    implemented get all posts route

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 var express = require('express');
 var server = express();
 var mongoose = require('mongoose');
+var postRouter = require('./server/routers/post.router.js');
 
 var port = process.env.PORT || 8080;
 var mongoURI = process.env.MONGOURI || require('./config.js').mongoURI;
 
 mongoose.connect(mongoURI); // establish connection to mongodb
+mongoose.Promise = require('bluebird'); // bluebird used for promises for mongoose data
 
 server.get('/', function(req, res) {
   res.send('I am working!');
 });
+
+server.use(postRouter);
 
 server.listen(port, function() {
   console.log('Now listening on port: ' + port);

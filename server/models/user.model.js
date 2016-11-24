914db02191f438b4b4cef13aeb6f0db84bf44fca
ksed <hydrogeo.coder@gmail.com>
2016-11-23 19:47:05 -0500

    adds user model

---
+++ server/models/user.model.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number,
+    min: 13,
+    max: 120,
+    required: true
+  }
+});
+
+var User = mongoose.model('User', userSchema);
+module.exports = User;

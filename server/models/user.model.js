d18a0d02d6955600173c168b11a55071f8c8c1bd
ksed <hydrogeo.coder@gmail.com>
2016-11-30 18:49:31 -0500

    implements secure password methods

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var crypto = require('crypto');
 
 var userSchema = new Schema({
   email: {
@@ -7,17 +8,29 @@ var userSchema = new Schema({
     required: true,
     unique: true
   },
-  password: {
-    type: String,
-    required: true
-  },
   age: {
     type: Number,
     min: 13,
     max: 120,
     required: true
+  },
+  hash: {
+    type: String
+  },
+  salt: {
+    type: String
   }
 });
+
+userSchema.methods.setPassword = function(password){
+  this.salt = crypto.randomBytes(16).toString('hex');
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+};
+userSchema.methods.validPassword = function(password){
+  var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+  return this.hash === hash;
+};
+userSchema.methods.generateJwt = function(){};
 
 var User = mongoose.model('User', userSchema);
 module.exports = User;

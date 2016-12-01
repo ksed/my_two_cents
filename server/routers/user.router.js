9050a765ada64da64fbed4fa3353ea75571378df
ksed <hydrogeo.coder@gmail.com>
2016-11-30 20:31:46 -0500

    implements login method

--- server/routers/user.router.js
+++ server/routers/user.router.js
@@ -34,7 +34,34 @@ router.post('/users/signup/', function(req, res) {
     });
   });
 });
-router.post('/users/login/', function(req, res) {});
+router.post('/users/login/', function(req, res) {
+  // edge case missing information on the Request
+  if(!req.body.email || !req.body.password) {
+    return res.status(401).json({
+      msg: 'Bad Request: the username or password you provided is incorrect'
+    });
+  }
+  User.findOne({email: req.body.email}, function(err, user) {
+    if(err) {
+      return res.status(500).json({
+        msg: err
+      });
+    }
+    if(!user) {
+      return res.status(401).json({
+        msg: 'Bad Request: the username or password you provided is incorrect'
+      });
+    }
+    if(!user.validPassword(req.body.password)) {
+      return res.status(401).json({
+        msg: 'Bad Request: the username or password you provided is incorrect'
+      });
+    }
+    return res.status(200).json({
+      token: user.generateJwt()
+    });
+  });
+});
 router.put('/users/profile/:userId', function(req, res) {});
 
 module.exports = router;

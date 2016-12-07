9be18380539d6cf1c90ad5020a7919d43d6efc95
ksed <hydrogeo.coder@gmail.com>
2016-12-06 19:27:58 -0500

    got the signup front end working

--- public/js/controllers/signup.controller.js
+++ public/js/controllers/signup.controller.js
@@ -2,9 +2,22 @@
   angular.module('two-cents')
     .controller('SignUpController', SignUpController);
 
-  SignUpController.$inject = [];
+  SignUpController.$inject = ['$scope', 'AuthService'];
 
-  function SignUpController(){
-    
+  function SignUpController($scope, AuthService){
+    $scope.signup = signup;
+    $scope.required = true;
+
+    function signup(newUser) {
+      console.log($scope.signupForm.input);
+      AuthService.signup(newUser)
+                .then(function() {
+                  alert('Yay! Signed up!');
+                })
+                .catch(function() {
+                  $scope.newUser = {};
+                  alert('Bad Request: Try again...');
+                });
+    }
   }
 }());

b439fc6bdbd24d91eaf4d3fadc4dc3a820b3421f
ksed <hydrogeo.coder@gmail.com>
2016-12-06 19:49:45 -0500

    add login service and front-end logic similar to signup

--- public/js/controllers/login.controller.js
+++ public/js/controllers/login.controller.js
@@ -2,9 +2,21 @@
   angular.module('two-cents')
     .controller("LoginController", LoginController);
 
-  LoginController.$inject = [];
+  LoginController.$inject = ['$scope', 'AuthService', '$location'];
 
-  function LoginController(){
-    
+  function LoginController($scope, AuthService, $location){
+    $scope.login = login;
+    $scope.required = true;
+
+    function login(user) {
+      AuthService.login(user)
+          .then(function() {
+            $location.path('/');
+          })
+          .catch(function() {
+            $scope.user = {};
+            $location.path('/login');
+          });
+    }
   }
 }());

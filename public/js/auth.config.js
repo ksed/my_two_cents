cc346f26592e5d86f60f7dc87736b755abfebec4
ksed <hydrogeo.coder@gmail.com>
2016-12-07 18:59:23 -0500

    adds auth configuration to limit going to private pages

--- public/js/auth.config.js
+++ public/js/auth.config.js
@@ -2,7 +2,13 @@
   angular.module('two-cents')
     .run(AuthConfig);
 
-  AuthConfig.$inject = [];
+  AuthConfig.$inject = ['$rootScope', '$location', 'AuthService'];
 
-  function AuthConfig(){}
+  function AuthConfig($rootScope, $location, AuthService) {
+    $rootScope.$on('$routeChangeStart', function(event, nextRoute, currentRoute) {
+      if( nextRoute.access.restricted && !AuthService.isLoggedIn() ) {
+        $location.path('/'); // goes home if not logged in and trying a protected route.
+      }
+    });
+  }
 }());

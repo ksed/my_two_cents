46710c60e75e7da8e51c1c6e43d691cc1c2d900a
ksed <hydrogeo.coder@gmail.com>
2016-12-06 19:33:53 -0500

    adjusts the signup to move to login

--- public/js/controllers/signup.controller.js
+++ public/js/controllers/signup.controller.js
@@ -2,21 +2,20 @@
   angular.module('two-cents')
     .controller('SignUpController', SignUpController);
 
-  SignUpController.$inject = ['$scope', 'AuthService'];
+  SignUpController.$inject = ['$scope', 'AuthService', '$location'];
 
-  function SignUpController($scope, AuthService){
+  function SignUpController($scope, AuthService, $location){
     $scope.signup = signup;
     $scope.required = true;
 
     function signup(newUser) {
-      console.log($scope.signupForm.input);
       AuthService.signup(newUser)
                 .then(function() {
-                  alert('Yay! Signed up!');
+                  $location.path('/login');
                 })
                 .catch(function() {
                   $scope.newUser = {};
-                  alert('Bad Request: Try again...');
+                  $location.path('/signup');
                 });
     }
   }
